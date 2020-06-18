66d9b006f7f0d4b6b93a2b40f9765247f6774bcd
Toni  Diep <adiep123@gmail.com>
2020-06-18 00:35:38 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,20 +7,20 @@ class Show < ActiveRecord::Base
     Show.order(rating: :desc).first
   end
 
-- `most_popular_show`: this method should return the show with the highest
-  rating. _hint_: use the `highest_rating` method as a helper method.
+# - `most_popular_show`: this method should return the show with the highest
+#   rating. _hint_: use the `highest_rating` method as a helper method.
 
-- `lowest_rating`: returns the lowest value in the ratings column.
+# - `lowest_rating`: returns the lowest value in the ratings column.
 
-- `least_popular_show`: returns the show with the lowest rating.
+# - `least_popular_show`: returns the show with the lowest rating.
 
-- `ratings_sum`: returns the sum of all of the ratings.
+# - `ratings_sum`: returns the sum of all of the ratings.
 
-- `popular_shows`: returns an array of all of the shows that have a rating
-  greater than `5`. _hint_: use the `where` Active Record method.
+# - `popular_shows`: returns an array of all of the shows that have a rating
+#   greater than `5`. _hint_: use the `where` Active Record method.
 
-- `shows_by_alphabetical_order`: returns an array of all of the shows sorted by
-  alphabetical order according to their names. _hint_: use the `order` Active
-  Record method.
+# - `shows_by_alphabetical_order`: returns an array of all of the shows sorted by
+#   alphabetical order according to their names. _hint_: use the `order` Active
+#   Record method.
   
 end
